f83c495921526dbb695141ccc25e3846d594be4a
Robert Giacinto <robert.giacinto@gmail.com>
2013-02-11 01:39:45 +0100

    splitting received command

--- src/main/scala/org/mudland/Implicits.scala
+++ src/main/scala/org/mudland/Implicits.scala
@@ -1,10 +1,11 @@
 package org.mudland
 
 import akka.actor.ActorSystem
-
+import java.nio.ByteBuffer
 /**
  * Implicit definitions for the game.
  */
 object Implicits {
   implicit val system = ActorSystem("mudland-system")
+  implicit def string2ByteBuffer(s: String) = ByteBuffer.wrap(s.getBytes("ASCII"))
 }
--- src/main/scala/org/mudland/Mudland.scala
+++ src/main/scala/org/mudland/Mudland.scala
@@ -1,14 +1,13 @@
 package org.mudland
 
 import scala.concurrent.duration._
-import akka.actor.{Props, ActorSystem}
+import akka.actor.{Props}
 import akka.pattern.ask
 import spray.util._
 import spray.io._
 import java.net.InetSocketAddress
 import spray.io.IOBridge.Key
 import org.mudland.Implicits._
-
 /**
  * Main Object that starts the game server.
  */
@@ -30,18 +29,31 @@ class GameServer extends IOServer {
   override def bound(endpoint: InetSocketAddress, bindingKey: Key, bindingTag: Any): Receive = {
     super.bound(endpoint, bindingKey, bindingTag) orElse {
       case IOBridge.Received(handle, buffer) =>
-        buffer.array.asString.trim match {
+        val tokens = buffer.array.asString.trim.split(" ").toList
+        val verb = tokens.head
+        verb match {
           case "STOP" =>
             ioBridge ! IOBridge.Send(handle, BufferBuilder("Shutting Down...").toByteBuffer)
             log.info("Shutting down MUDland...")
             context.system.shutdown()
           case x =>
-            log.info("Received {}, echoing!", x)
+            log.info("Received {} from {}, echoing!", x, handle)
             ioBridge ! IOBridge.Send(handle, buffer, None)
         }
+
+
       case IOBridge.Closed(_, reason) =>
         log.info("Connection closed. Reason: {}", reason)
 
     }
   }
+
 }
+
+object GameServer{
+  sealed trait GameServerCommand
+  case class TextMessage(text: String) extends GameServerCommand
+}
+
+
+
