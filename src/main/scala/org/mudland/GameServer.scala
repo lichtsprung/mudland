0f844198dcec3f936a77200861e78792298a26d9
Robert Giacinto <robert.giacinto@gmail.com>
2013-02-15 16:48:16 +0100

    chat command

--- src/main/scala/org/mudland/GameServer.scala
+++ src/main/scala/org/mudland/GameServer.scala
@@ -1,34 +1,44 @@
 package org.mudland
 
 import spray.io._
+import spray.util._
 import akka.actor.{ActorRef, Props}
 import java.net.InetSocketAddress
 import spray.io.IOBridge.{Key, Connection}
 import collection.mutable
 import java.nio.ByteBuffer
-import org.mudland.GameServer.HandlePlayerAction
+import org.mudland.GameServer.{BroadcastMessage, HandlePlayerAction}
 
 /**
  * Game server that handles the network requests and forwards them to the actor that is handling the player instance.
  */
 class GameServer extends IOServer {
   val ioBridge = IOExtension(context.system).ioBridge()
-  val registry = context.actorOf(Props[PlayerRegistry], "registry")
-  var connections = mutable.HashMap[Connection, ActorRef]()
+  var connections = mutable.Map[Connection, ActorRef]()
 
   override def bound(endpoint: InetSocketAddress, key: Key, tag: Any): Receive = {
     case IOBridge.Received(handle, buffer) =>
-      connections.get(handle).get ! HandlePlayerAction(buffer)
+      val tokens = buffer.array.asString.trim.split(" ").toList
+      val verb = tokens.head
+      verb match {
+        case "login" if tokens.tail.size > 0 =>
+          log.info("Logging in {}", tokens.tail.head)
+          val playerHandler = context.actorOf(Props[PlayerHandler])
+          playerHandler ! GameServer.Register(handle, tokens.tail.head)
+          connections += handle -> playerHandler
+          sendMessage("Welcome,  " + tokens.tail.head + "\n", handle)
+        case _ => connections.get(handle).foreach(c => c ! HandlePlayerAction(buffer))
+      }
+    case BroadcastMessage(text, name) => connections.keys.foreach(connection => sendMessage(name + " says: " + text, connection))
     case IOBridge.Connected(key, tag) =>
       val handle = createConnection(key, tag)
       sender ! IOBridge.Register(handle)
-      val playerHandler = context.actorOf(Props[PlayerHandler])
-      playerHandler ! GameServer.Register(handle)
-      connections += handle -> playerHandler
+      sendMessage("Hello, stranger! Who are you? (Use: login <name>)\n", handle)
     case IOBridge.Closed(_, reason) =>
       log.info("Connection closed. Reason: {}", reason)
-
   }
+
+  def sendMessage(text: String, handle: Connection) = ioBridge ! IOBridge.Send(handle, BufferBuilder(text + "\n").toByteBuffer)
 }
 
 
@@ -38,8 +48,10 @@ object GameServer {
 
   case class HandlePlayerAction(buffer: ByteBuffer)
 
-  case class Register(connection: Connection)
+  case class Register(connection: Connection, username: String)
 
   case class TextMessage(text: String, name: String) extends GameServerCommand
+
+  case class BroadcastMessage(text: String, name: String) extends GameServerCommand
 
 }
--- src/main/scala/org/mudland/Mudland.scala
+++ src/main/scala/org/mudland/Mudland.scala
@@ -1,19 +1,13 @@
 package org.mudland
 
 import scala.concurrent.duration._
-import akka.actor.{ActorRef, ActorLogging, Actor, Props}
+import akka.actor.{ActorLogging, Actor, Props}
 import akka.pattern.ask
 import spray.util._
 import spray.io._
-import java.net.InetSocketAddress
-import spray.io.IOBridge.Key
 import org.mudland.Implicits._
+import org.mudland.GameServer.{BroadcastMessage, Register, HandlePlayerAction}
 import collection.mutable
-import org.mudland.PlayerRegistry._
-import org.mudland.PlayerRegistry.PlayerConnectionRequest
-import org.mudland.PlayerRegistry.RegisterPlayer
-import org.mudland.GameServer.{Register, HandlePlayerAction, TextMessage}
-import java.nio.ByteBuffer
 
 /**
  * Main Object that starts the game server.
@@ -22,45 +16,67 @@ object Mudland extends App {
   val gameServer = system.actorOf(Props[GameServer], "game-server")
 
   gameServer
-    .ask(IOServer.Bind("localhost", 8888))(1 second span)
+    .ask(IOServer.Bind("openplexus", 16333))(1 second span)
     .onSuccess {
     case IOServer.Bound(endpoint, _) =>
-      println("MUDland server started on port 8888")
+      println("MUDland server started on port 16333")
   }
 }
 
 class PlayerHandler extends Actor with ActorLogging {
   val ioBridge = IOExtension(context.system).ioBridge()
   var handle: Option[Connection] = None
-  var registry = context.actorFor("/user/registry")
+  val player = Player("no name")
 
   def receive = {
     case HandlePlayerAction(buffer) =>
       val tokens = buffer.array.asString.trim.split(" ").toList
       val verb = tokens.head
-      verb match {
-        case "STOP" =>
-          ioBridge ! IOBridge.Send(handle.get, BufferBuilder("Shutting Down...").toByteBuffer)
-          log.info("Shutting down MUDland...")
-          context.system.shutdown()
-        case "login" =>
-          log.info("Changing name to {}", tokens.tail.head)
-        case x =>
-          log.info("Received {} from {}, echoing!", x, handle)
-          sendMessage(x)
-      }
-    case Register(connection) => handle = Option[Connection](connection)
+      val params = tokens.tail ::: List(player.username)
+      log.info("params: {}", params)
+      player.commands.get(verb).foreach(command => command._2(params))
+    case Register(connection, username) =>
+      handle = Some(connection)
+      player.username = username
   }
 
-  def sendMessage(text: String) = ioBridge ! IOBridge.Send(handle.get, BufferBuilder(text).toByteBuffer)
+  def sendMessage(text: String) = handle.foreach(c => ioBridge ! IOBridge.Send(c, BufferBuilder(text + "\n").toByteBuffer))
 }
 
 
-class Player(var name: String, var connection: Connection) {
+class Player(var username: String) {
+  var name = "no name"
+  var attributes = Map[String, Int](
+    "vitality" -> 10,
+    "strength" -> 10,
+    "dexterity" -> 10,
+    "intelligence" -> 10
+  )
+  var commands = mutable.HashMap[String, GameCommands.GameCommand](
+    GameCommands.SAY._1 -> GameCommands.SAY,
+    GameCommands.SHUTDOWN._1 -> GameCommands.SHUTDOWN
+  )
 }
 
 object Player {
-  def apply(name: String, connection: Connection) = new Player(name, connection)
+  def apply(name: String) = new Player(name)
 }
+
+
+object GameCommands {
+  type GameCommand = (String, (List[String]) => Unit)
+
+  val SAY = ("say", (params: List[String]) => {
+    val stringBuilder = mutable.StringBuilder.newBuilder
+    params.reverse.drop(1).reverse.foreach(s => stringBuilder.append(s + " "))
+    Mudland.gameServer ! BroadcastMessage(stringBuilder.toString(), params.last)
+  })
+
+  val SHUTDOWN = ("shutdown", (params: List[String]) => {
+    Mudland.gameServer ! BroadcastMessage("Shutting down...", params.tail.head)
+    system.shutdown()
+  })
+}
+
 
 
